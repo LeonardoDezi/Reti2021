0149850c0dbe38a28beb852d2bd8503966a8bf20
drob05 <10576279@polimi.it>
2021-11-22 18:05:18 +0100

    Revert "Update 10588151_10576279.vhd"
    This reverts commit 2649e14da92f07acf0618c01b96d070e31c3a96a.

--- 10588151_10576279.vhd
+++ 10588151_10576279.vhd
@@ -48,14 +48,26 @@ architecture Behavioral of project_reti_logiche is
   
   --Segnale per il registro per il pixel considerato
   signal pixel: std_logic_vector(7 downto 0);
+
+  --Segnale per il pixel temporaneo
+  signal pixel_temp: std_logic_vector(7 downto 0);
+
+  --Segnale per il nuovo pixel
+  signal new_pixel: std_logic_vector(7 downto 0);
   
-  --Segnale minimo e massimo valore
-  signal max: std_logic_vector(7 downto 0);
-  signal min: std_logic_vector(7 downto 0);
+  --Segnali minimo e massimo valore
+  signal max_v: std_logic_vector(7 downto 0);
+  signal min_v: std_logic_vector(7 downto 0);
+
+  --Segnali per lo shift value
+  signal delta: std_logic_vector(7 downto 0);
+  signal floor_v: std_logic_vector(7 downto 0);
+  signal shift_temp: std_logic_vector(7 downto 0);
+  signal shift_level: integer;
 
 begin
 
-    process(i_clk, i_rst, current_state)
+    process(i_clk, i_rst, next_state)
     begin
         if(rising_edge(i_clk)) then
             if(i_rst = '1') then
@@ -96,7 +108,7 @@ begin
 				next_state <= S_CALC_ADDR;
 			when S_CALC_ADDR =>
 			    count <= offset;
-				if (offset ="0000000000000000")then
+				if (offset ="0000000000000000") then
 					next_state <= S_DONE;
 				else
 					next_state <= S_LOAD_PIXEL;
@@ -113,6 +125,16 @@ begin
 				end if;
 			when S_MIN_MAX =>
 			    current_address_next <= current_address + "0000000000000001";
+				if (current_address = "0000000000000010") then
+					max_v <= pixel;
+					min_v <= pixel;
+				else
+					if (pixel > max_v)then
+						max_v <= pixel;
+					elsif (pixel < min_v) then
+						min_v <= pixel;
+					end if ;
+				end if ;
 			    count_next <= count - "0000000000000001"; 
 				if (count = "0000000000000000") then
 					next_state <= S_SHIFT;
@@ -120,24 +142,57 @@ begin
 					next_state <= S_LOAD_PIXEL;				
 				end if ;
 			when S_SHIFT =>
+				current_address <= current_address_next;
+			    count <= count_next;
+				delta <= max_v - min_v;
+				if (delta = "00000000") then
+					floor_v <= "00000000";
+				elsif (delta >= "00000001" and delta <= "00000010") then
+					floor_v <= "00000001";
+				elsif (delta >= "00000011" and delta <= "00000110") then
+					floor_v <= "00000010";
+				elsif (delta >= "00000111" and delta <= "00001110") then
+					floor_v <= "00000011";
+				elsif (delta >= "00001111" and delta <= "00011110") then
+					floor_v <= "00000100";
+				elsif (delta >= "00011111" and delta <= "00111110") then
+					floor_v <= "00000101";
+				elsif (delta >= "00111111" and delta <= "01111110") then
+					floor_v <= "00000110";
+				elsif (delta >= "01111111" and delta <= "11111110") then
+					floor_v <= "00000111";
+				elsif (delta = "11111111") then
+					floor_v <= "00001000";					
+				end if;
+				shift_temp <= "00001000" - delta;
+				shift_level <= TO_INTEGER(unsigned(shift_temp));
 				next_state <= S_LOAD_PIXEL;
 			when S_NEW_PIXEL =>
+				current_address_next <= current_address + "0000000000000001";
+				pixel_temp <= std_logic_vector(shift_left(unsigned(pixel-min_v), shift_level));
+				if (pixel_temp < "11111111") then
+					new_pixel <= pixel_temp;
+				else
+					new_pixel <= "11111111";
+				end if ;
 			    o_en <= '1';
 			    o_we <= '1';
 				next_state <= S_SAVE_PIXEL;
 			when S_SAVE_PIXEL =>
-			    o_address <= current_address + base_address;
-			    o_data <= pixel;
+				current_address <= current_address_next;
+			    o_address <= current_address;
+			    o_data <= new_pixel;
 				if (current_address = offset) then
 					next_state <= S_DONE;
 				elsif (current_address /= offset) then
 					next_state <= S_LOAD_PIXEL;
 				end if ;
 			when S_DONE =>
-				if (i_start = '1')then
+				o_done <= '0';
+				if (i_start = '1') then
 					next_state <= S_DONE;
 				else
-				next_state <= S_RST;
+					next_state <= S_RST;
 				end if;
 		end case;
 	end process;
